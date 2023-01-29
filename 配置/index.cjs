0c3cc2ff58d1b0e611839461cd4af064d8225595
sangjuasdasd <123855812+sangjuasdasd@users.noreply.github.com>
2023-01-29 14:05:37 +0800

    Update and rename config/index.cjs to 配置/index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -8,56 +8,51 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx6d4392622303d6df',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'wx6d4392622303d6df',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '江苏',
+  CITY: '南京',
 
-  USERS: [
+  USERS: [okGg-5q6U_-BVc3ruvsveMqUW890
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '考研人',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '	7VVauQTnsLU8Q6k-ZtUDWMkcewfSgJoXHd-4atlXWJw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '09-16',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '殷可欣', year: '200', date: '09-16',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '考试', name: '考研', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '开始', name: '努力的日子', year: '2023', date: '01-13',
         },
       ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
+      // 我已经努力了有xxxx天了的配置
+      customizedDateList: 
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '	7VVauQTnsLU8Q6k-ZtUDWMkcewfSgJoXHd-4atlXWJw',
 
-  CALLBACK_USERS: [
+  CALLBACK_USERS: [okGg-5ukdWeEWPYXLdsdNeCqeDcg
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'okGg-5ukdWeEWPYXLdsdNeCqeDcg',
     }
   ],
 
